510cf67342d27d16e7b940f6158ef7470ae6bd1d
mogetta <muzigen@gmail.com>
2020-05-19 12:21:46 +0900

    fix layout

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -4,7 +4,7 @@ import {FLContent} from 'ng-flamelink';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.sass']
 })
 export class BlogComponent implements OnInit {
 
