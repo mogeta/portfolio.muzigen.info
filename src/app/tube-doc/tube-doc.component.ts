bfd0b71d4c0faf3dff22fd2102eae68bcdbcd4bf
mogetta <muzigen@gmail.com>
2020-05-19 18:47:59 +0900

    udpate youtube list

--- src/app/tube-doc/tube-doc.component.ts
+++ src/app/tube-doc/tube-doc.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FLContent} from 'ng-flamelink';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-tube-doc',
@@ -7,22 +8,27 @@ import {FLContent} from 'ng-flamelink';
   styleUrls: ['./tube-doc.component.css']
 })
 export class TubeDocComponent implements OnInit {
-
-  public projects = this.content.valueChanges({
-    schemaKey: 'yt_mbChannlel',
-    // filters: [['category', '==', 'web']]
-  });
+  private targetID = '';
+  public projects;
 
   constructor(
-    private content: FLContent
+    private content: FLContent,
+    private route: ActivatedRoute,
   ) {
-    console.log(this.projects);
+
   }
 
   ngOnInit(): void {
     const tag = document.createElement('script');
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
+
+    this.targetID = this.route.snapshot.queryParams.target;
+    console.log(this.targetID);
+    this.projects = this.content.valueChanges({
+      schemaKey: this.targetID,
+      // filters: [['category', '==', 'web']]
+    });
   }
 
 }
